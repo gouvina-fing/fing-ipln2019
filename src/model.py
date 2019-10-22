1c42e0aec017fca0a6c214a9b367ce3daddf01ac
gouvina <gouvina@kona.tech>
2019-10-21 22:22:40 -0300

    Fixed embeddings files

--- src/model.py
+++ src/model.py
@@ -73,7 +73,7 @@ class Model():
         self.classifier = pickle.load(open(const.MODEL_FOLDER + const.MODEL_FILE, 'rb'))
 
     # Constructor
-    def __init__(self, vectorization=const.VECTORIZERS['word_embeddings'], model='nb', evaluation=const.EVALUATIONS['none'], data_path=const.DATA_FOLDER, params={}):
+    def __init__(self, vectorization=const.VECTORIZERS['word_embeddings'], model='mlp_classifier', evaluation=const.EVALUATIONS['none'], data_path=const.DATA_FOLDER, params={}):
 
         # Create empty dataset for training
         self.dataset = None
